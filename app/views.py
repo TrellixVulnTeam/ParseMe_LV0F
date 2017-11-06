ca79de5d8aeeadeb45941a11251cdf88e7f24349
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-11-06 07:15:43 +0000

    add test parser / string

--- app/views.py
+++ app/views.py
@@ -1,21 +1,32 @@
-from flask import render_template
+import feedparser
+import logging
+from html.parser import HTMLParser
+import requests
+from flask import render_template, flash
 from app import app
 
+#@app.context_processor
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     project = {'name' : 'UNICORN AI BLOG CHECKER'}
     blogs = [ #MACHINE LEARNING AT WORK!!
         {
             'author' : {'username' : 'THE ZUCC'},
+            'blogtitle' : 'LIVE FROM MENLO PARK',
             'body' : 'Hello, fellow Human Beings. Have you tried VR?'
         },
         {
             'author' : {'username' : 'Joe'},
+            'blogtitle' : 'HIRE ME',
             'body' : 'give me the Z U C C'
         }
     ]
+    userProfile = feedparser.parse('https://medium.com/feed/@joewmcdaniel')
+
+    dog = 'hi how are you'
     return render_template('index.html',
-                           title='Home',
                            project=project,
-                           blogs=blogs)
+                           blogs=blogs,
+                           dog=dog,
+                           userProfile=userProfile)
